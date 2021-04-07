7dca4f4ecd03c9749e4755df76c2f28b658834c9
Your Github User Name <david.lav.williams@gmail.com>
2021-04-07 11:14:37 -0500

    blasters provider is started

--- src/blasters/blasterProvider.js
+++ src/blasters/blasterProvider.js
@@ -1,55 +1,28 @@
 import React, { useState } from "react"
 
-export const AnimalContext = React.createContext()
+export const BlasterContext = React.createContext()
 
-export const AnimalProvider = (props) => {
-    const [Animals, setAnimals] = useState([])
-    const [searchTerms, setSearchTerms]=useState("")
-    const getAnimals = () => {
-        return fetch("http://localhost:8088/animals")
+export const BlasterProvider = (props) => {
+    const [Blasters, setBlasters] = useState([])
+
+    const getBlasters = () => {
+        return fetch("http://localhost:8088/blasters")
             .then(res => res.json())
-            .then(setAnimals)
-    }
-    const addAnimal = animal => {
-        return fetch("http://localhost:8088/animals", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(animal)
-        })
-            .then(getAnimals)
-    }
-    const getAnimalById = (id) => {
-        return fetch(`http://localhost:8088/animals/${ id }?_expand=location&_expand=customer`)
-            .then(res => res.json())
-    }
-
-    const releaseAnimal = animalId => {
-        console.log(animalId)
-        return fetch(`http://localhost:8088/animals/${animalId}`, {
-            method: "DELETE"
-        })
-            .then(getAnimals)
-    }
-
-    const updateAnimal = animal => {
-        return fetch(`http://localhost:8088/animals/${animal.id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(animal)
-        })
-            .then(getAnimals)
+            .then(setBlasters)
     }
     
+    // const getBlasterByName = (id) => {
+    //     return fetch(`http://localhost:8088/animals/${ id }?_expand=location&_expand=customer`)
+    //         .then(res => res.json())
+    // }
+
+    
     return (
-        <AnimalContext.Provider value={{
-            Animals, addAnimal, getAnimals, getAnimalById, searchTerms, setSearchTerms, releaseAnimal, updateAnimal
+        <BlasterContext.Provider value={{
+            Blasters, setBlasters, getBlasters
         }}>
             {props.children}
-        </AnimalContext.Provider>
+        </BlasterContext.Provider>
 
     )
     
