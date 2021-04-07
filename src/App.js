d48e5bd6a6e6630de119ed96d63bca777e4911f5
Your Github User Name <david.lav.williams@gmail.com>
2021-04-07 11:39:04 -0500

    the navbar is working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { ApplicationViews } from './ApplicationViews';
+import { NavBar } from './nav/navBar';
+import { Route, Redirect } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
      <ApplicationViews></ApplicationViews>
     </div>
   );
---
+++ src/nav/navBar.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./navBar.css"
+
+export const NavBar = (props) => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/">Blaster</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/animals">Laser Sords</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/customers">Cart</Link>
+            </li>
+        </ul>
+    )
+}
